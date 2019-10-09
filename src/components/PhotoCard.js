0fb27f5abe99e76d2e541e405be0243ec0d81c39
JChartier001 <JChartier001@gmail.com>
2019-10-09 15:27:46 -0500

    Added some styling

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -6,8 +6,8 @@ const PhotoCard = props => {
         <div className ="photo-card" >
 
             <h1 className="App-logo" className="App-header">NASA Photo of the Day</h1>
-            <p>Date: {props.date}</p>
-            <p>Explanation: {props.explanation}</p>
+            <p className="date">Date: {props.date}</p>
+            <p className="explanation"> {props.explanation}</p>
             <img className="dailyPhoto" alt='NASA Photo of the Day' src={props.hdurl}/>;
         </div>
 
